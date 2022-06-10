76e75226a205fba204dd5cc75ba821f44ceb8e57
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-06-10 13:06:30 +0530

    Update config.php

--- Sign In With Google/config.php
+++ Sign In With Google/config.php
@@ -11,12 +11,6 @@ require_once 'vendor/autoload.php';
 //Make object of Google API Client for call Google API
 $google_client = new Google\Client();
 
-//Set the OAuth 2.0 Client ID
-// $google_client->setClientId('588905532108-i00ub0it6gh08hndfd6g8g1fsseu82h1.apps.googleusercontent.com');
-
-//Set the OAuth 2.0 Client Secret key
-// $google_client->setClientSecret('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');
-
 $google_client->setAuthConfig('client_secret_588905532108-i00ub0it6gh08hndfd6g8g1fsseu82h1.apps.googleusercontent.com.json');
 
 
